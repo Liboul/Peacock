1420c4b033ce58b652b2d18676181366ccb8752b
Benjamin Combourieu <benjamin@talent.io>
2016-07-07 11:12:39 +0000

    [RPI_SOFTWARE]: testing a led works!

--- rpi_software/lib/test_led.rb
+++ rpi_software/lib/test_led.rb
@@ -1,16 +1,15 @@
 require 'pi_piper'
 
 class TestLed
-  include PiPiper
+  class << self
+    include PiPiper
 
-  def self.call(pin)
-    watch :pin => pin do
-      puts "Pin changed from #{last_value} to #{value}"
+    def call(pin)
+      gpio_pin = PiPiper::Pin.new(:pin => pin.to_i, :direction => :out)
+      gpio_pin.on
+      sleep(2)
+      gpio_pin.off
+      File.open('/sys/class/gpio/unexport', 'w') {|f| f.write("#{pin.to_i}")}
     end
-
-    pin = PiPiper::Pin.new(:pin => pin, :direction => :out)
-    pin.on
-    pin.sleep(5)
-    pin.off
   end
 end
--- rpi_software/rpi_software.rb
+++ rpi_software/rpi_software.rb
@@ -1,12 +1,13 @@
 require 'sinatra/base'
-require 'lib/test_led'
+require './lib/test_led'
 
 class RpiSoftwareApp < Sinatra::Base
   get '/hi' do
     'leeeeooooooon leeeeeeeeooooon'
   end
 
-  get 'test_led/:pin' do
+  get '/test_led/:pin' do
     TestLed.call(params[:pin])
+    'yo'
   end
 end
